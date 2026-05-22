b44f5392ffacc271846c330bcaa619ef85e7e1ea
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2026-05-22 12:30:47 +0700

    add README

--- obj/Debug/net10.0/AI-SEO-Ssas-Platform.AssemblyInfo.cs
+++ obj/Debug/net10.0/AI-SEO-Ssas-Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI-SEO-Ssas-Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca3d02f7ba7724413d8497b85c9739d63a69c80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c22c91f56261ce559f319f219dbda0c157b8eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI-SEO-Ssas-Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI-SEO-Ssas-Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
